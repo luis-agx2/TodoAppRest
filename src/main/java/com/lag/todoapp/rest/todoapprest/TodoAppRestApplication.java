8a6cffe740b183379405bbff7061f1dbbdbb0961
Luis Alejandro Garcia <luisalejandrogarciax2@gmail.com>
2024-06-17 23:51:36 -0600

    Init commit

---
+++ src/main/java/com/lag/todoapp/rest/todoapprest/TodoAppRestApplication.java
@@ -0,0 +1,13 @@
+package com.lag.todoapp.rest.todoapprest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lag/todoapp/rest/todoapprest/TodoAppRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lag.todoapp.rest.todoapprest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
